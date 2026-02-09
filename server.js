d4ff99d28e25d4187ec0487630d02d58c89bb906
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-09 11:03:10 -0300

    atualizando

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-'use-strict'
+'use strict'
 
 const http = require("http"); 
 const debug = require("debug") ("nodestr:server");
 const express = require("express"); 
-const { version } = require("os");
 
 const app = express(); 
 const port = 4000;  
@@ -17,4 +16,8 @@ let route = router.get("/",(req,res,next)=>{
 res.status(200).send({title:"Node App",version:"0.0.1"}); 
 });
 
-app.use("/",route); 
+app.use("/",router); 
+
+server.listen(port); 
+console.log("Servidor no ar"); 
+
