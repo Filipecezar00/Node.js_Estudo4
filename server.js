9190f87b4daaa96bde92b075f2bddce11dbf17ed
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-09 11:19:01 -0300

    atualizando

--- server.js
+++ server.js
@@ -5,19 +5,28 @@ const debug = require("debug") ("nodestr:server");
 const express = require("express"); 
 
 const app = express(); 
-const port = 4000;  
+const port = normalizePort(process.env.PORT || "3000"); 
 app.set("port",port);  
 
 
 const server = http.createServer(app); 
 const router = express.Router(); 
 
-let route = router.get("/",(req,res,next)=>{
+router.get("/",(req,res,next)=>{
 res.status(200).send({title:"Node App",version:"0.0.1"}); 
 });
 
 app.use("/",router); 
 
+function normalizePort(val){
+    const port = parseInt(val,10); 
+    if(isNaN(port)) return val;  
+    if(port>=0) return port;  
+    return false; 
+}
+
 server.listen(port); 
 console.log("Servidor no ar"); 
+
+
 
