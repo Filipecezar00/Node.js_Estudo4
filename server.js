30ef1d8da6a86e2f289b865b93bc306eca23956d
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-09 10:53:51 -0300

    atualizando

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const http = require("http"); 
 const debug = require("debug") ("nodestr:server");
 const express = require("express"); 
+const { version } = require("os");
 
 const app = express(); 
 const port = 4000;  
@@ -11,3 +12,9 @@ app.set("port",port);
 
 const server = http.createServer(app); 
 const router = express.Router(); 
+
+let route = router.get("/",(req,res,next)=>{
+res.status(200).send({title:"Node App",version:"0.0.1"}); 
+});
+
+app.use("/",route); 
