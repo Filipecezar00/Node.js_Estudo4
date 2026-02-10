e8b17de17fb76afc15a6719860f43ee48afe71b1
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-10 11:37:27 -0300

    atualizando

--- license/app.js
+++ license/app.js
@@ -15,12 +15,22 @@ router.get("/",(req,res,next)=>{
     }); 
 }); 
 
-router.post("/",(req,res,next)=>{
+const create = router.post("/",(req,res,next)=>{
     res.send(req.body); 
 }); 
 
+const put = router.put("/:id",(req,res,next)=>{
+    const id = req.params.id; 
+    res.status(200).send({id:id, item:req.body}); 
+}); 
+
+const del = router.delete("/:id",(req,res)=>{
+    const id = req.params.id; 
+    res.status(200).send(id) 
+}); 
 
 app.use("/",router);  
 app.use("/products",create); 
+app.use("/:id",put); 
 
 module.exports = app; 
