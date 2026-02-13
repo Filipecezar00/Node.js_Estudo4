3a158bc2d48276da59bc1dc69e97d4e1fe2115d3
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-13 15:10:10 -0300

    atualizando

--- license/app.js
+++ license/app.js
@@ -1,9 +1,14 @@
 'use strict'
+
 const express = require("express");  
 const bodyparser = require("body-parser"); 
+const moongose = require("mongoose"); 
 
 const app = express(); 
 const router= express.Router(); 
+
+//Conecta ao Banco 
+moongose.connect(process.env.MONGO_URL);  
 
 //Carregando Rotas 
 const index = require('../src/routes/index'); 
@@ -14,5 +19,8 @@ app.use(bodyparser.urlencoded({extended:false}));
 
 app.use("/",index);   
 app.use("/products",products);  
+
+
+
 
 module.exports = app; 
