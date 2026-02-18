bfedbe4fb8bed3c95aeca50d86f9b0ca480e6e3f
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-18 16:13:34 -0300

    atualizando

--- src/controllers/product.controler.js
+++ src/controllers/product.controler.js
@@ -58,6 +58,19 @@ exports.post = (req,res,next) =>{
     
 }
 
+exports.getByTag = (req,res,next)=>{
+Product.find({
+    tags:req.params.tag,
+    active:true
+},'title description price slug tags')
+.then(data=>{
+res.status(200).send(data); 
+})
+.catch(erro=>{
+res.status(400).send(erro); 
+})
+}
+
 exports.put = (req,res,next) =>{
     const id = req.params.id
     res.status(200).send({id:id,item:req.body}); 
