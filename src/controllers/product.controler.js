9b99574a38ac54cd88f831978f7ce554c962230b
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-01 13:15:00 -0300

    atualizando

--- src/controllers/product.controler.js
+++ src/controllers/product.controler.js
@@ -3,9 +3,10 @@
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 const ValidationContract = require("../validators/fluentValidator");
+const Repository = require("../repositories/productRepository");
 
 exports.get = (req, res, next) => {
-  Product.find({ active: true }, "title price slug")
+  Repository.get()
     .then((data) => {
       res.status(200).send(data);
     })
@@ -99,7 +100,7 @@ exports.post = (req, res, next) => {
     "A descrição precisa conter no mínimo três caracteres",
   );
 
-  if (contract.isValid) {
+  if (!contract.isValid()) {
     res.status(400).send(contract.errors()).end();
     return;
   }
