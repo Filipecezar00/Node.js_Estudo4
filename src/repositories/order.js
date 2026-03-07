1bdcbf1ee7ffba6a499b2494d74a1f0104eb0a6e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-07 14:18:39 -0300

    atualizando

--- src/repositories/order.js
+++ src/repositories/order.js
@@ -8,6 +8,6 @@ exports.create = async (data) => {
 };
 
 exports.get = async (data) => {
-  var res = await Order.find({}).populate("customer").populate("itens.product");
+  var res = await Order.find({}).populate("customer").populate("items.product");
   return res;
 };
