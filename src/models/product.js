67e360a871225cd46fb897ed16a1ed1df539278e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-09 09:48:47 -0300

    atualizando

--- src/models/product.js
+++ src/models/product.js
@@ -1,41 +1,46 @@
-'use strict'; 
+"use strict";
 
-const mongoose = require("mongoose"); 
-const Schema = mongoose.Schema; 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  tags: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
 
-    title:{
-        type:String,
-        required:true,
-        trim:true
-    }, 
-    slug:{
-        type:String,
-        required:true,
-        trim:true,
-        index:true,
-        unique:true
-    }, 
-    description:{
-        type:String,
-        required:true,
-        trim:true
-    }, 
-    price:{
-        type:Number,
-        required:true 
-    }, 
-    active:{
-        type:Boolean,
-        required:true,
-        default:true 
-    }, 
-    tags:[{
-        type:String,
-        required:true
-    }]
-
-}); 
-
-module.exports = mongoose.model("Product",schema); 
+module.exports = mongoose.model("Product", schema);
