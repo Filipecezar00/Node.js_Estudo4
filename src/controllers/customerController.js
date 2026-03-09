f8dc5103fa69b18d0fc6241d30a4c96e0f694dde
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-09 11:30:18 -0300

    atualizando

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -8,40 +8,63 @@ const authService = require("../services/authService");
 const emailService = require("../services/emailService");
 
 exports.post = async (req, res, next) => {
-  exports.authenticate = async (req, res, next) => {
-    try {
-      const customer = await repository.authenticate({
-        email: req.body.email,
-        password: md5(req.body.password + global.SALT_KEY),
-      });
-      authService.generateToken({ email: customer.email, name: customer.name });
+  let contract = new validationContract();
+  contract.hasMinLen(req.body.name, 3, "O nome precisa conter três caracteres");
+  contract.isEmail(req.body.email, "O e-mail está inválido");
+  contract.hasMinLen(
+    req.body.password,
+    5,
+    "A senha precisa de no mínimo cinco caracteres",
+  );
 
-      if (!customer) {
-        res.status(404).send({
-          message: "Usuário ou senha invalidos",
-        });
-        return;
-      }
-      const token = await authService.generateToken({
+  if (!contract.isValid()) {
+    return res.status(400).send(contract.errors());
+  }
+
+  try {
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + config.SALT_KEY),
+    });
+    res.status(201).send({ message: "Cliente cadastrado com Sucesso!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Falha ao cadastrar cliente" });
+  }
+};
+exports.authenticate = async (req, res, next) => {
+  try {
+    const customer = await repository.authenticate({
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY),
+    });
+
+    if (!customer) {
+      return res.status(404).send({
+        message: "Usuário ou senha invalidos",
+      });
+    }
+    const token = await authService.generateToken({
+      id: customer._id,
+      email: customer.email,
+      name: customer.name,
+    });
+    res.status(201).send({
+      token: token,
+      data: {
         email: customer.email,
         name: customer.name,
-      });
-      res.status(201).send({
-        token: token,
-        data: {
-          email: customer.email,
-          name: customer.name,
-        },
-      });
+      },
+    });
 
-      res.status(200).send("Usuario Cadastrado no sistema");
-    } catch (err) {
-      res
-        .status(500)
-        .send(
-          "Erro, Não foi possivel cadastrar o usuario devido a um erro interno.",
-        );
-      console.error(err);
-    }
-  };
+    res.status(200).send("Usuario Cadastrado no sistema");
+  } catch (err) {
+    res
+      .status(500)
+      .send(
+        "Erro, Não foi possivel cadastrar o usuario devido a um erro interno.",
+      );
+    console.error(err);
+  }
 };
