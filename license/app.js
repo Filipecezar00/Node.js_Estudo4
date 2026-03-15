25cefa94d0b6daebd27fabb67fcb5d945f5a390f
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-15 10:14:12 -0300

    atualizando

--- license/app.js
+++ license/app.js
@@ -21,8 +21,22 @@ const RouteProducts = require("../src/routes/product.js");
 const RouteCustomer = require("../src/routes/customerRoutes.js");
 const RouteOrder = require("../src/routes/orderRoute.js");
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "5mb",
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-access-token",
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
 
 app.use("/", Routeindex);
 
